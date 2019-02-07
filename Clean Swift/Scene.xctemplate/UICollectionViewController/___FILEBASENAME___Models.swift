8c463a68b9d0d43c0b4004656bcdfb0d27ef709d
Riley Usagi <riley-usagi@users.noreply.github.com>
2019-02-07 12:06:09 +0300

    Update ___FILEBASENAME___Models.swift

--- Clean Swift/Scene.xctemplate/UICollectionViewController/___FILEBASENAME___Models.swift
+++ Clean Swift/Scene.xctemplate/UICollectionViewController/___FILEBASENAME___Models.swift
@@ -4,8 +4,16 @@ import Magic
 enum ___VARIABLE_sceneName___ {
   
   enum Something {
-    struct Request {}
-    struct Response {}
-    struct ViewModel {}
+    struct Request {
+      
+    }
+    
+    struct Response {
+      
+    }
+    
+    struct ViewModel {
+      
+    }
   }
 }
